9a0e1434ec451ef850cff4a70cfeaa5fa177bcc2
Dennis Golomazov <dennis@trueskills.com>
2016-02-22 11:37:02 -0800

    Support Django 1.9 in incorporating django native admin, otherwise it won't work

--- mongoadmin/__init__.py
+++ mongoadmin/__init__.py
@@ -5,10 +5,16 @@ from mongoadmin.sites import site
 from django.conf import settings
 
 if getattr(settings, 'MONGOADMIN_OVERRIDE_ADMIN', False):
+    from django import get_version as get_django_version
+    from distutils.version import StrictVersion
     import django.contrib.admin
+
     # copy already registered model admins
     # without that the already registered models
     # don't show up in the new admin
-    site._registry = django.contrib.admin.site._registry
-    
-    django.contrib.admin.site = site 
+    if StrictVersion(get_django_version()) < StrictVersion('1.9'):
+        site._registry = django.contrib.admin.site._registry
+        django.contrib.admin.site = site
+    else:
+        site._registry = django.contrib.admin.sites.site._registry
+        django.contrib.admin.sites.site = site
